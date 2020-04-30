760538fe5b7716f1d09b879bfa1c8520889901e6
tayyab <tayyab.shahzada@dativa.com>
2020-04-30 16:44:23 +0100

    override the quickstart bucket param, no longer dependant on the Turbine one create a bucket and upload lambda code as well as scripts used for initialising the EC2 machines wrapped variables in {}

--- project_deploy.sh
+++ project_deploy.sh
@@ -6,17 +6,17 @@ echo 'running'
 STAGE="TESTING"
 PROFILE="airflow-sandbox"
 REGION="us-east-1"
-REGION_OPT=" --region $REGION"
+REGION_OPT=" --region ${REGION}"
 
 
-if [[ ! -z $PROFILE ]]; then
-    # $PROFILE was given
-    PROFILE_OPT=" --profile $PROFILE"
+if [[ ! -z ${PROFILE} ]]; then
+    # ${PROFILE} was given
+    PROFILE_OPT=" --profile ${PROFILE}"
     echo ''
-    echo "Using: '$PROFILE_OPT'"
+    echo "Using: '${PROFILE_OPT}'"
     # Create credential envars
-    export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id $PROFILE_OPT)
-    export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key $PROFILE_OPT)
+    export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id ${PROFILE_OPT})
+    export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key ${PROFILE_OPT})
 fi
 
 
@@ -30,13 +30,16 @@ S3_PACKAGE_KEY="packages"
 PACKAGED_TEMPLATES="templates_packaged"
 
 
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account' $PROFILE_OPT $REGION_OPT)
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account' ${PROFILE_OPT} ${REGION_OPT})
 DEPLOY_BUCKET="${PROJECT}-deploy-${AWS_ACCOUNT_ID}-${REGION}"
-LAMBDA_CODE_BUCKET="${PROJECT}-lambda-code-${AWS_ACCOUNT_ID}-${REGION}"
 
+TURBINE_BUCKET="${PROJECT}-turbine-deloyment-${AWS_ACCOUNT_ID}-${REGION}"
+TURBINE_PREFIX=""
 
-export AWS_DEFAULT_REGION=$REGION
-aws configure set default.region $AWS_DEFAULT_REGION
+PARAM_OVERRIDES="QSS3BucketName=${TURBINE_BUCKET} QSS3KeyPrefix=${TURBINE_PREFIX}"
+
+export AWS_DEFAULT_REGION=${REGION}
+aws configure set default.region ${AWS_DEFAULT_REGION}
 
 check_for_error() {
 
@@ -54,29 +57,31 @@ check_for_error() {
 echo ''
 echo "CREATING TEMPLATES..."
 
-# Create Bucket for lambda code
-aws s3 mb s3://$LAMBDA_CODE_BUCKET $PROFILE_OPT $REGION_OPT
-cp functions/load_metric.py load_metric.py
+# Create Bucket for lambda code and to store scripts for setting up airflow
+aws s3 mb s3://${TURBINE_BUCKET} ${PROFILE_OPT} ${REGION_OPT}
+# Zip and upload the lambda code ready for deploment
 zip load_metric functions/load_metric.py
-aws s3 cp load_metric.zip s3://$LAMBDA_CODE_BUCKET/ $PROFILE_OPT
+aws s3 cp load_metric.zip s3://${TURBINE_BUCKET}/${TURBINE_PREFIX}functions ${PROFILE_OPT}
+# Upload scripts used for airflow initialisation on EC2 machines
+aws s3 cp scripts/ s3://${TURBINE_BUCKET}/${TURBINE_PREFIX}scripts --recursive ${PROFILE_OPT}
 
 # Create deploy bucket if it doesn't already exist
-aws s3 mb s3://$DEPLOY_BUCKET $PROFILE_OPT $REGION_OPT
+aws s3 mb s3://${DEPLOY_BUCKET} ${PROFILE_OPT} ${REGION_OPT}
 
 
-for template in ./$TEMPLATE_KEY/*.$TEMPLATE_EXT; do
+for template in ./${TEMPLATE_KEY}/*.${TEMPLATE_EXT}; do
     echo ''
-    echo "PACKAGING: $template"
+    echo "PACKAGING: ${template}"
     package_template="${template/${TEMPLATE_KEY}/${PACKAGED_TEMPLATES}}"
     echo ${package_template}
-    aws cloudformation package --template-file $template --s3-bucket $DEPLOY_BUCKET --s3-prefix $S3_PACKAGE_KEY --output-template-file $package_template $PROFILE_OPT $REGION_OPT
+    aws cloudformation package --template-file ${template} --s3-bucket ${DEPLOY_BUCKET} --s3-prefix ${S3_PACKAGE_KEY} --output-template-file ${package_template} ${PROFILE_OPT} ${REGION_OPT}
 done
 check_for_error $? "Failed to create package"
 
 # Deploy or update the AWS infrastructure
 echo ''
 echo "...CREATING/UPDATING CLOUDFORMATION STACKS..."
-aws cloudformation deploy --template-file $ROOT_TEMPLATE --s3-bucket $DEPLOY_BUCKET --s3-prefix $TEMPLATE_KEY --stack-name $STACK_NAME --capabilities CAPABILITY_NAMED_IAM $PROFILE_OPT $REGION_OPT
+aws cloudformation deploy --template-file ${ROOT_TEMPLATE} --s3-bucket ${DEPLOY_BUCKET} --s3-prefix ${TEMPLATE_KEY} --stack-name ${STACK_NAME} --parameter-overrides ${PARAM_OVERRIDES} --capabilities CAPABILITY_NAMED_IAM ${PROFILE_OPT} ${REGION_OPT}
 check_for_error $? "Failed to deploy template"
 
 

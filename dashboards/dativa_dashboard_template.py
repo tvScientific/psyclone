283416bf9ffe34e24a5479fbac25cd4e35f06073
abhishekrao <abhishek.rao@dativa.com>
2020-10-01 11:01:42 +0100

    added attribute to dashboard template for queue alphanum

--- dashboards/dativa_dashboard_template.py
+++ dashboards/dativa_dashboard_template.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-(c) Dativa 2019, all rights reserved
+(c) Deductive 2019, all rights reserved
 -----------------------------------------
 This code is licensed under MIT license
 
@@ -13,7 +13,7 @@ are permitted provided that the following conditions are met:
     * Redistributions in binary form must reproduce the above copyright notice,
     this list of conditions and the following disclaimer in the documentation
     and/or other materials provided with the distribution.
-    * Neither the name of the Dativa Limited nor the names of
+    * Neither the name of the Deductive Limited nor the names of
     its contributors may be used to endorse or promote products derived from
     this software without specific prior written permission.
 
@@ -42,7 +42,7 @@ from troposphere import sns
 """
 Initialise logger
 """
-logger = logging.getLogger("dativa.tools.aws.dashboard")
+logger = logging.getLogger("deductive.tools.aws.dashboard")
 
 
 class DativaDashboardTemplate:
@@ -62,6 +62,7 @@ class DativaDashboardTemplate:
         # Note: Module names should use '_' not '-' but some names cannot contain '_' so replace with dashes
         self._project = project_name
         self._dash_project = self._project.replace('_', '-')
+        self._alphanum_project = self._project.replace('_', '').replace('-', '')
 
         self._file_ext = 'json'
         self._file_ext_output = 'template'
